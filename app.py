7ddf5ec120b5690553d45cd4c8c4b238a21a4cc7
wywsmail <wywsmail@gmail.com>
2024-07-16 13:39:36 +0800

    update

--- app.py
+++ app.py
@@ -1,12 +1,21 @@
 from flask import Flask
+from flask_restful import Resource, Api
 
 app = Flask(__name__)
+api = Api(app)
 
 
-@app.route("/")
-def hello_world():
-    return "Hello World Heroku!"
+# @app.route("/")
+# def hello_world():
+#     return "Hello World Heroku!"
 
+
+class HelloWorld(Resource):
+    def get(self):
+        return {"data": "Hello World Heroku!"}
+
+
+api.add_resource(HelloWorld, "/")
 
 if __name__ == "__main__":
     app.run()

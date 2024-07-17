37d0222b8c3e437b4b405cf97b1ab96402afb45c
wywsmail <wywsmail@gmail.com>
2024-07-17 11:34:25 +0800

    add User get and post

--- app.py
+++ app.py
@@ -1,13 +1,10 @@
-from flask import Flask
+from flask import Flask, request
 from flask_restful import Resource, Api
 
 app = Flask(__name__)
 api = Api(app)
 
-
-# @app.route("/")
-# def hello_world():
-#     return "Hello World Heroku!"
+user_list = []
 
 
 class HelloWorld(Resource):
@@ -15,7 +12,32 @@ class HelloWorld(Resource):
         return {"data": "Hello World Heroku!"}
 
 
+class User(Resource):
+    def get(self, username):
+        """
+        get user detail information
+        """
+        for user in user_list:
+            if user["username"] == username:
+                return user
+        return {"message": "User not found"}, 404
+
+    def post(self, username):
+        """
+        create a new user
+        """
+        password = request.json.get("password")
+        print(password)
+        if password:
+            user = {"username": username, "password": password}
+            user_list.append(user)
+            return {"message": "User created"}, 201
+        return {"message": "Password is required"}, 400
+
+
 api.add_resource(HelloWorld, "/")
+api.add_resource(User, "/user/<string:username>")
+
 
 if __name__ == "__main__":
     app.run()
